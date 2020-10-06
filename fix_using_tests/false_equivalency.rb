9209a9b40ff29635033fc322ee20b10c05db736c
Alan sanchez <asanchezhaza@gmail.com>
2020-10-06 09:44:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -2,7 +2,7 @@
 
 def get_user_input
   gets.chomp
-  binding.pry
+  
 end
 
 def prompt_user
@@ -13,8 +13,10 @@ end
 
 def selection(num)
   if num = 1
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
+  binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
 end
